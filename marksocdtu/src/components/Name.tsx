5e438e702eed17589f7f6404fa616fde34ec40e8
Yash-Choudhary6 <123976594+Yash-Choudhary6@users.noreply.github.com>
2023-12-29 04:28:58 -0800

    render

--- marksocdtu/src/components/Name.tsx
+++ marksocdtu/src/components/Name.tsx
@@ -3,13 +3,20 @@ import Image from "next/image";
 import l from "@/assets/landing.png";
 import {motion} from 'framer-motion';
 
-const Name= () => {
+type Props = {
+    name: string;
+    designation: string;
+    image : string;
+};
+
+
+const Name= (props: Props) => {
     return(
     
              <motion.div whileHover={{scale:1.5}} className="flex flex-col h-1/6 w-1/6 border flex-shrink-0  hover:shadow-xl">
-                            <div className="flex border flex-shrink-0 bg-blue rounded">man</div>
-                            <div className="flex  border flex-shrink-0" ><Image src={l} alt="logo" /></div>
-                            <div className="flex border flex-shrink-0">designation</div>
+                            <div className="flex border flex-shrink-0 text-center text-white font-semibold justify-center bg-dark rounded-b-full">{props.name}</div>
+                            <div className="flex   flex-shrink-0" ><Image src={l} alt="logo" /></div>
+                            <div className="flex  flex-shrink-0 text-center  font-bold justify-center">{props.designation}</div>
 
             </motion.div>    
         
